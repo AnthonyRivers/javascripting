70736bc78f6356fdc0a417b17def90b6d937dfef
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-28 11:55:10 -0700

    committing the second tutorial of NodeSchool javascripting on declaring and assigning variables. A screenshot of the verification is also committed.

---
+++ variables.js
@@ -0,0 +1,7 @@
+/* Antonio Rios
+ * May 27, 2015
+ * Second tutorial from NodeSchool Javascripting
+ * showing how to declare and assign a variable.
+*/
+var example = 'some string';
+console.log(example);
