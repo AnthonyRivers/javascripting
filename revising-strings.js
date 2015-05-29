a1098ab49104c4912da0691de3017924c4298a41
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-28 20:21:37 -0700

    Committing the 5th tutorial of javascripting on the concept of using the replace method on a string object.

---
+++ revising-strings.js
@@ -0,0 +1,13 @@
+/* Antonio Rios
+ * May 27, 2015
+ * 5th tutorial of 
+ * javascripting from NodeSchool
+ * On the concept of using the 
+ * replace method on a string object.
+*/
+
+var example = 'this example exists';
+example = example.replace('exists', 'is awesome');
+//console.log(example);
+example = 'pizza is wonderful';
+console.log(example);
