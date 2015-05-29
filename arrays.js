faa0d55ed2c5fad39d83645d8d946335f91ded71
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-29 16:16:13 -0700

    Committing the 11th javascripting tutorial from NodeSchool on the concept of arrays.

---
+++ arrays.js
@@ -0,0 +1,10 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 11th javascripting tutorial
+ * from NodeSchool covering the
+ * concept of arrays.
+ */
+
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+
+console.log(pizzaToppings);
