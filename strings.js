a8855f4c237feae60a3038777e6fe7eefa33c9fd
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-29 12:04:33 -0700

    Added the header comment on the strings.js document.

--- strings.js
+++ strings.js
@@ -1,2 +1,7 @@
+// Antonio Rios
+// May 27, 2015
+// Third tutorial of javascripting NodeSchool
+/////////////////////////////////////////////////
+
 var someString = 'this is a string';
 console.log(someString);
