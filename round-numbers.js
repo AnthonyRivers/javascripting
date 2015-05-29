9fc96f6d88c8eef8bb89d8af984d8ca2d356de01
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-29 12:03:45 -0700

    committing the 7th javascripting NodeSchool tutorial covering the concept of using the round() method from the the Math module.

---
+++ round-numbers.js
@@ -0,0 +1,12 @@
+/* Antonio Rios
+ * May 27, 2015
+ * 7th javascripting tutorial
+ * from NodeSchool covering
+ * the concept of using the 
+ * method round() in the Math module.
+ */
+
+var roundUp = 1.5;
+var rounded = Math.round(roundUp);
+
+console.log(rounded);
