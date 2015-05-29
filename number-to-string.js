89582aa3cfde26fcd1aa454e9a89ffa6be6d5eb8
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-29 12:07:12 -0700

    committing the 8th javascripting tutorial from NodeSchool covering the conept of converting a number to a string type using the toString() method.

---
+++ number-to-string.js
@@ -0,0 +1,13 @@
+/* Antonio Rios
+ * May 27, 2015
+ * 8th javascripting tutorial
+ * from NodeSchool covering the
+ * concept of converting a number
+ * to a string type using
+ * the toString() method.
+ */
+
+var n = 128;
+n = n.toString();
+
+console.log(n);
