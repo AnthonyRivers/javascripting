f74e5511cdb0a001f36e38d26850011057b55589
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-29 16:13:45 -0700

    committing the 10th javascripting tutorial from NodeSchool on the concept of using a for loop.

---
+++ for-loop.js
@@ -0,0 +1,16 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 10th javascripting tutorial
+ * from NodeSchool o the concept
+ * of using a for loop.
+*/
+
+
+var total = 0;
+var limit = 10;
+
+for(var i = 0; i < limit; i++){
+    total += i;
+   
+}
+console.log(total);
