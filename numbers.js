b05c7eb9ce6b17617fb34f4c92d5b240f3f70aec
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-28 20:23:40 -0700

    Committing the 6th javascripting tutorial on the concept of using number literals.

---
+++ numbers.js
@@ -0,0 +1,10 @@
+/* Antonio Rios
+ * May 27, 2015
+ * 6th javascripting tutorial
+ * from NodeSchool on the concept
+ * of using number literals.
+*/
+
+var example = 123456789;
+console.log(example);
+
