02a74a5e1ad54f3a3ff8fbe88f41bd598d6fd07e
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-29 12:55:21 -0700

    committing the 9th javascripting tutorial from NodeSchool on the concept of using conditional statements.

---
+++ if-statement.js
@@ -0,0 +1,14 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 9th javascripting tutorial
+ * from NodeSchool on the concept
+ * of using conditional statements.
+ */
+var fruit = 'orange';
+
+if (fruit.length > 5) {
+    console.log("The fruit name has more than five characters.");}
+else {
+    console.log("The fruit name has five characters or less.");
+}
+
