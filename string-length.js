93b11475a55b48fce53b194843a3f583823b109d
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-28 17:32:52 -0700

    Committing the 4th tutorial on using the length method of a string object.

---
+++ string-length.js
@@ -0,0 +1,11 @@
+/* Antonio Rios
+ * May 27, 2015
+ * Javascripting tutorial from NodeSchool
+ * 4th tutorial on using the length method of a String object
+*/
+
+var example = 'example string'; 
+example.length;
+
+console.log(example.length);
+
