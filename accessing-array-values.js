a592dbc3346b0a9fe3e70439450291d3a1ee34a2
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-30 09:31:45 -0700

    committing the 13th javascripting tutorial from NodeSchool on the concept of accessing items in an array.

---
+++ accessing-array-values.js
@@ -0,0 +1,9 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 13th javascripting tutorial 
+ * from NodeSchool on the concept
+ * of accessing items in an array.
+ */
+
+var food = ['apple', 'pizza', 'pear'];
+console.log(food[1]);
