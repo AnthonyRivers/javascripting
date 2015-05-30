6609a2ec6407984bdb6c4401574363ca7fcdd422
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-30 12:13:56 -0700

    Committing the 16th javascripting tutorial from NodeSchool on the concept of accessing an object propertie.

---
+++ object-properties.js
@@ -0,0 +1,12 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 16th javascripting tutorial 
+ * from NodeSchool on the concept
+ * of accessing object properties.
+ */
+
+var food = {
+    types: 'only pizza'
+};
+
+console.log(food.types);
