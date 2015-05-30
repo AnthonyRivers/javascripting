09481fb65bde705def01027120ae77fda0b046ef
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-30 09:41:04 -0700

    committing the 15th javascripting tutorial from NodeSchool on the concept of creating objects.

---
+++ objects.js
@@ -0,0 +1,14 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 15th javascripting tutorial
+ * from NodeSchool on the concept
+ * of creating objects.
+ */
+
+var pizza = {
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+    crust: 'deep dish',
+    serves: 2
+}
+
+console.log(pizza);
