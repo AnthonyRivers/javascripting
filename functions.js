d9dbf03bff3e4d9843e921cc13a129cc6c90c1cf
Antonio Rios <antonio.rios.jr@gmail.com>
2015-05-30 12:20:56 -0700

    Committing the 17th javascripting tutorial from NodeSchool on the concept of creating functions.

---
+++ functions.js
@@ -0,0 +1,12 @@
+/* Antonio Rios
+ * May 28, 2015
+ * 17th javascripting tutorial
+ * from NodeSchool on the concept
+ * of using creating functions.
+ */
+
+function eat(food) {
+    return food + ' tasted really good.';
+}
+console.log(eat('bananas'));
+
